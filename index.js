6962119d1febd3b9d926b6a0932380e0b5d9d5f0
Paula Fleck <paulakfleck@gmail.com>
2020-06-24 21:00:20 -0300

    First express.js API

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express'),
-    path = require('path'),
-    PORT = process.env.PORT || 5000;
+	path = require('path'),
+	PORT = process.env.PORT || 5000;
 
-express()
-//   .use(express.static(path.join(__dirname, 'src')))
-  .set('views', path.join(__dirname, 'src'))
-//   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('main/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+var app = express(),
+	car = { type: "Fiat", model: "500", color: "white" };
+
+app
+	.get('/', (req, res) => res.json(car))
+	.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+// app.set('view engine', 'html');
